4e005be46c2c1bc463c9a8be531da914a59e9d29
Ardash55 <ardash30042005g@gmail.com>
2025-10-27 22:42:09 +0300

    Создан класс ThisDay, куда перенесены приемы пищи и методы их добавления

--- src/main/java/org/example/CalorieApp.java
+++ src/main/java/org/example/CalorieApp.java
@@ -12,16 +12,13 @@ import java.io.FileInputStream;
 
 public class CalorieApp {
     static User user = new User(0, 0, 0, "", "", 0, 0);
-    static Meals breakfast = new Meals("Завтрак", 0);
-    static Meals lunch = new Meals("Обед", 0);
-    static Meals dinner = new Meals("Ужин", 0);
-    static double totalEatenCalories;
+    static ThisDay thisDay = new ThisDay();
     static double freeCalories;
     public static void start() {
         while (true) {
-            freeCalories = user.getCalorieNorm() - totalEatenCalories;
+            freeCalories = user.getCalorieNorm() - thisDay.getTotalEatenCalories();
             Scanner sc = new Scanner(System.in);
-            System.out.println("Съеденно калорий - " + totalEatenCalories);
+            System.out.println("Съеденно калорий - " + thisDay.getTotalEatenCalories());
             System.out.println("Осталось калорий - " + freeCalories);
             System.out.println("Выберите действие");
             System.out.println("1. Зарегистрироватья");
@@ -33,7 +30,7 @@ public class CalorieApp {
             switch (choice) {
                 case 1 -> createUser();
                 case 2 -> calculateCalorieIntake();
-                case 3 -> eat();
+                case 3 -> thisDay.eat();
             }
         }
     }
@@ -222,57 +219,4 @@ public class CalorieApp {
         }
         System.out.println(user.getCalorieNorm());
     }
-
-    public static void eat() {
-        Scanner sc = new Scanner(System.in);
-        while (true)
-            try {
-                System.out.println("Выберите прием пищи");
-                System.out.println("1. Завтрак");
-                System.out.println("2. Обед");
-                System.out.println("3. Ужин");
-                int mealChoise = sc.nextInt();
-                switch (mealChoise) {
-                    case 1 -> addFoodToMeal(breakfast);
-                    case 2 -> addFoodToMeal(lunch);
-                    case 3 -> addFoodToMeal(dinner);
-                }
-
-                if(mealChoise != 1 && mealChoise != 2 && mealChoise != 3) {
-                    System.out.println("Выберите один из трех приемов пищи");
-                    continue;
-                }
-
-                break;
-            } catch (Exception e) {
-                System.out.println("Введите число");
-                sc.nextLine();
-            }
-    }
-
-    public static void addFoodToMeal(Meals meal) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите название блюда:");
-        String mealName = sc.nextLine();
-        System.out.println("Введите количество калорий:");
-        while (true) {
-            try {
-                int mealCalories = sc.nextInt();
-                FoodTime newFoodTime = new FoodTime(mealName, mealCalories);
-                meal.addFood(newFoodTime);
-                totalEatenCalories += mealCalories;
-
-                if(mealCalories <= 0) {
-                    System.out.println("Калорий должно быть больше нуля");
-                    continue;
-                }
-
-                break;
-            } catch (Exception e) {
-                System.out.println("Введите числом количество калорий");
-                sc.nextLine();
-            }
-        }
-    }
-
 }
---
+++ src/main/java/org/example/ThisDay.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import javax.xml.crypto.Data;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class ThisDay implements Serializable  {
+    private LocalDate today;
+    private Meals breakfast;
+    private Meals lunch;
+    private Meals dinner;
+    private double totalEatenCalories;
+    private ArrayList<FoodTime> foodTimes;
+
+    ThisDay() {
+        this.today = LocalDate.now();
+        this.breakfast = new Meals("Завтрак", 0);
+        this.lunch = new Meals("Обед", 0);
+        this.dinner = new Meals("Ужин", 0);
+        this.totalEatenCalories = 0;;
+        this.foodTimes = new ArrayList<>();
+    }
+
+    double getTotalEatenCalories() {
+        return totalEatenCalories;
+    }
+
+    void eat() {
+        Scanner sc = new Scanner(System.in);
+        while (true)
+            try {
+                System.out.println("Выберите прием пищи");
+                System.out.println("1. Завтрак");
+                System.out.println("2. Обед");
+                System.out.println("3. Ужин");
+                int mealChoise = sc.nextInt();
+                switch (mealChoise) {
+                    case 1 -> addFoodToMeal(this.breakfast);
+                    case 2 -> addFoodToMeal(this.lunch);
+                    case 3 -> addFoodToMeal(this.dinner);
+                }
+
+                if(mealChoise != 1 && mealChoise != 2 && mealChoise != 3) {
+                    System.out.println("Выберите один из трех приемов пищи");
+                    continue;
+                }
+
+                break;
+            } catch (Exception e) {
+                System.out.println("Введите число");
+                sc.nextLine();
+            }
+    }
+
+    void addFoodToMeal(Meals meal) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите название блюда:");
+        String mealName = sc.nextLine();
+        System.out.println("Введите количество калорий:");
+        while (true) {
+            try {
+                int mealCalories = sc.nextInt();
+                FoodTime newFoodTime = new FoodTime(mealName, mealCalories);
+                meal.addFood(newFoodTime);
+                this.totalEatenCalories += mealCalories;
+
+                if(mealCalories <= 0) {
+                    System.out.println("Калорий должно быть больше нуля");
+                    continue;
+                }
+
+                break;
+            } catch (Exception e) {
+                System.out.println("Введите числом количество калорий");
+                sc.nextLine();
+            }
+        }
+    }
+ }
